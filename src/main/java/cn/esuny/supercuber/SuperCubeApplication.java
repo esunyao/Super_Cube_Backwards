97ec2638dd0fb5f338a219e5926cff86bd373b2e
Esuny <esun.yao@hotmail.com>
2024-10-19 12:40:49 +0800

    initial

---
+++ src/main/java/cn/esuny/supercuber/SuperCubeApplication.java
@@ -0,0 +1,13 @@
+package cn.esuny.supercuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperCubeApplication.class, args);
+    }
+
+}
